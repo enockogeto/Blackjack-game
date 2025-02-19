348561fdae0ccb8b383233d41b28c590b329ebb7
enockogeto <enockogeto1@gmail.com>
2025-02-19 17:55:52 +0300

    changes to the javascript

--- script.js
+++ script.js
@@ -1,40 +1,64 @@
-let firstCard = 8
-let secondCard = 10
-let cards = [firstCard,secondCard]
-
-let sum = firstCard + secondCard
+let cards = []
+let sum = 0
 
 let winner = false
 let isalive = true
 
-let sumel = document.querySelector("#sum-el")
-let messageEl = document.querySelector("#message-el")
 let message = " "
 
+let player ={
+    name: "eones",
+    dimes: 100
+}
+document.querySelector("#player-el").textContent= player.name +": $"+ player.dimes
+function randomnumber(){
+    let randomN = Math.floor(Math.random()*13) + 1
+    if(randomN > 10){ return 10}
+    else if(randomN === 1){ return 11}
+    else{return randomN}
+}
+
 function startgame(){
+    let firstCard = randomnumber()
+    let secondCard = randomnumber()
+    cards =[firstCard,secondCard]
+    sum = firstCard + secondCard
+
     rendergame()
 }
+
 function rendergame(){
-    document.getElementById("cards").textContent = "Cards: " +cards[0] +"," +cards[1]
-    sumel.textContent = "Sum: " + sum
+
+    document.querySelector("#cards").textContent = "Cards: " + cards
+    document.querySelector("#sum-el").textContent = "Sum: " + sum
+
     if(sum <= 20){
         message ="draw another card"
     }
     else if(sum === 21){
         message = "You've won the game!"
          winner = true
+         isalive = false
     }
     else{
         message ="you're out of the game!"
         isalive = false
        }
 
- messageEl.textContent = message
+       document.querySelector("#message-el").textContent = message
 }
 
 function newcard(){
-  let pickcard = 1
-  document.getElementById("newcard").textContent = "New Card: " +pickcard
-  sum += pickcard 
-  rendergame()()
+    if(isalive === true && winner === false){
+        let pickcard = randomnumber()
+        document.getElementById("newcard").textContent = "New Card: " +pickcard
+        sum += pickcard 
+        cards.push(pickcard)
+
+       rendergame()
+    }
+}
+
+function resetgame(){
+    
 }
